ac48f575abc390dff6d6878f73428406bf0e8d4a
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-02-18 02:13:40 +1100

    editing local storage

--- app.js
+++ app.js
@@ -90,7 +90,7 @@ function clearItems() {
   container.classList.remove("show-container");
   displayAlert("empty list", "danger");
   setBackToDefault();
-  //   localStorage.removeItem('list');
+  localStorage.removeItem("list");
 }
 
 //delete function
@@ -146,7 +146,16 @@ function removeFromLocalStorage(id) {
   });
   localStorage.setItem("list", JSON.stringify(items));
 }
-function editLocalSotrage(id, vlaue) {}
+function editLocalSotrage(id, value) {
+  let items = getLocalStorage();
+  items = items.map(function (item) {
+    if (item.id === id) {
+      item.value = value;
+    }
+    return item;
+  });
+  localStorage.setItem("list", JSON.stringify(items));
+}
 function getLocalStorage() {
   return localStorage.getItem("list")
     ? JSON.parse(localStorage.getItem("list"))
